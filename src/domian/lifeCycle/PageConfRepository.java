6571f5f210ee5666eff02bbd395f9dbbad2ef3e4
Antinomy <antinomy.net@126.com>
2015-06-16 18:19:28 +0800

    impl yaml config style file for config page elments

--- src/domian/lifeCycle/PageConfRepository.java
+++ src/domian/lifeCycle/PageConfRepository.java
@@ -1,11 +1,8 @@
 package domian.lifeCycle;
 
-import com.esotericsoftware.yamlbeans.YamlException;
 import com.esotericsoftware.yamlbeans.YamlReader;
 
-import java.io.File;
 import java.io.FileReader;
-import java.io.InputStream;
 import java.util.Map;
 
 /**
@@ -20,11 +17,11 @@ public class PageConfRepository {
         Object object = reader.read();
         System.out.println(object);
 
-        map = (Map)object;
+        map = (Map) object;
     }
 
-    public String getConf(String page ,String element) {
-        Map targetPage = (Map)map.get(page);
+    public String getConf(String page, String element) {
+        Map targetPage = (Map) map.get(page);
         return targetPage.get(element).toString();
     }
 }
